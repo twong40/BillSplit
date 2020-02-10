2e145a257e25d5a5d17692ad2ad82d7a88be6aab
thomaswong1000@gmail.com <twong40@ucsc.edu>
2020-02-09 22:25:59 -0800

    Updates

---
+++ precache-manifest.e903d2077943feb824f42bcaeb6e5b2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9aa1055ed5ff438ee6a27ca296aa0f",
+    "url": "/twong40/BillSplit/index.html"
+  },
+  {
+    "revision": "4830f3c1eb287a22bfb7",
+    "url": "/twong40/BillSplit/static/css/main.fd70f2c8.chunk.css"
+  },
+  {
+    "revision": "1ab192273c6fe54c457d",
+    "url": "/twong40/BillSplit/static/js/2.abeb9220.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/twong40/BillSplit/static/js/2.abeb9220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4830f3c1eb287a22bfb7",
+    "url": "/twong40/BillSplit/static/js/main.23d02e50.chunk.js"
+  },
+  {
+    "revision": "d3499ae389ef6670976d",
+    "url": "/twong40/BillSplit/static/js/runtime-main.e1a515ce.js"
+  }
+]);
